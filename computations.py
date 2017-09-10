df79006d1fbf42beef5e27d6381416bb3677a48e
Luv Iyer <luviyer@gmail.com>
2017-09-10 12:13:47 -0400

    Creates dataframe

--- computations.py
+++ computations.py
@@ -1,2 +1,14 @@
 import pandas as pd
 
+def create_df(excel_file):
+    xl = pd.ExcelFile(excel_file)
+    df = xl.parse('Returns by Period')
+    df['Period'] = pd.to_datetime(df['Period'], format='%m/%d/%Y')
+    df.set_index('Period', inplace=True)
+
+    return df
+excel_file = '1Stock20062016.xls'
+
+df = create_df(excel_file)
+
+print(df.head())
