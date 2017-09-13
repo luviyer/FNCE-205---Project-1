d7d1dc1d34dab999492984f3f406169cd80557b9
Luv Iyer <luviyer@gmail.com>
2017-09-12 20:12:33 -0400

    All graphs and data, with proper plotting

--- computations.py
+++ computations.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 def create_df(excel_file, period):
     xl = pd.ExcelFile(excel_file)
     df = xl.parse('Values by Period')
-    # Insert the initial investment into the dataframe in order to complete monthly returns.
+
+    # Insert the initial investment into the dataframe in order to compute monthly returns.
     df.loc[-1] = ['12/31/{}'.format(period[0:4]), 10000, 10000, 10000]
     df.index = df.index + 1  # Shift the inserted row to the top
     df = df.sort_index()  # Sort by index
@@ -12,7 +13,8 @@ def create_df(excel_file, period):
     # Compute the monthly returns from the values at the end of each period.
     df = df.fillna(method='ffill')
     df[['A1', 'A2', 'A3']] = df[['A1', 'A2', 'A3']]/df[['A1', 'A2', 'A3']].shift(1) - 1
-    # Remove the Market data and the first row.
+
+    # Remove the Market data and the first row, which is NaN.
     df = df.loc[1:, ['Period', 'A1', 'A2', 'A3']]
 
     df['Period'] = pd.to_datetime(df['Period'], format='%m/%d/%Y')
@@ -23,7 +25,7 @@ def create_df(excel_file, period):
 # Create a bunch of dataframes for the n values in a time period
 # and put them into a multi-index frame
 n_values = [1, 2, 4, 10, 20, 30, 40, 70, 100]
-ten_year_period = ['20062016']
+ten_year_period = ['19861996']
 pn = pd.DataFrame()
 
 total_returns = {}
@@ -34,8 +36,11 @@ for period in ten_year_period:
     for n in n_values:
         excel_file = '{}/{}Stock{}.xls'.format(period, n, period)
         df = create_df(excel_file, period)
+
+        # Add 1 to the time series and take a cumulative product to set up geometric mean calculation.
         add_1 = df + 1
         cum_prod = add_1.cumprod()
+
         data = [{'ar_mean': df['High Cap'].mean(), 'std': df['High Cap'].std(),
                  'geo_mean': cum_prod.iloc[-1]['High Cap']**(1.0/df.shape[0]) - 1},
                 {'ar_mean': df['Mid Cap'].mean(), 'std': df['Mid Cap'].std(),
@@ -44,9 +49,10 @@ for period in ten_year_period:
                  'geo_mean': cum_prod.iloc[-1]['Low Cap']**(1.0/df.shape[0]) - 1}]
 
         stats = pd.DataFrame(data, index=['High Cap', 'Mid Cap', 'Low Cap'])
-        corr_matrix = df[['High Cap', 'Mid Cap', 'Low Cap']].corr()
+        corr_matrix = df.corr()
 
         total_returns['N={}'.format(n)] = df
+        stats.index.name = ''
         total_stats['N={}'.format(n)] = stats
         total_corr['N={}'.format(n)] = corr_matrix
 
@@ -54,7 +60,22 @@ for period in ten_year_period:
     total_stats = pd.concat(total_stats.values(), keys=total_stats.keys())
     total_corr = pd.concat(total_corr.values(), keys=total_corr.keys())
 
-print(total_returns)
-print(total_stats)
-print(total_corr)
+# print(total_returns)
+# print(total_stats)
+# print(total_corr)
+
+total_stats.unstack(level=1)['geo_mean'].plot()
+plt.title('Portfolio Geometric Mean ' + period[0:4] + '-' + period[4:])
+plt.savefig('graphs/geo_' + period[0:4] + '.png', dpi=600)
+plt.close()
+
+total_stats.unstack(level=1)['ar_mean'].plot()
+plt.title('Portfolio Arithmetic Mean ' + period[0:4] + '-' + period[4:])
+plt.savefig('graphs/ar_' + period[0:4] + '.png', dpi=600)
+plt.close()
+
+total_stats.unstack(level=1)['std'].plot()
+plt.title('Portfolio Standard Deviation ' + period[0:4] + '-' + period[4:])
+plt.savefig('graphs/std_' + period[0:4] + '.png', dpi=600)
+plt.close()
 
